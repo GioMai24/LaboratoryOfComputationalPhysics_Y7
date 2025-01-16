4cc4f1b254e64e465d3cd90b0970f308fbaf166b
GioMai24 <giovanniandrea.maida@studenti.unipd.it>
2025-01-16 17:24:11 +0100

    something

--- Lab_3/script_Bash.sh
+++ Lab_3/script_Bash.sh
@@ -2,48 +2,32 @@
 
 
 # Create students directory if it doesn't exist
-if [ ! -d "students" ]
-then
-	mkdir students
-fi
+if [ ! -d "students" ]; then mkdir students; fi
 
 
 # Download file if it doesn't exist
-if [ ! -f "LCP_students.csv" ]
-then
+if [ ! -f "students/LCP_students.csv" ]; then
 	wget -O "LCP_students.csv" "https://www.dropbox.com/scl/fi/bxv17nrbrl83vw6qrkiu9/LCP_22-23_students.csv?rlkey=47fakvatrtif3q3qw4q97p5b7&e=1"
 	cp "LCP_students.csv" "students/LCP_students.csv"
-else
-	echo "The file already exists: it won't be downloaded nor copied in the students directory"
-fi
+else echo "The file already exists: it won't be downloaded nor copied in the students directory"; fi
 
 
 
 cd "students"
 
 
-if [ ! -f "PoD.txt" ]
-then
-	touch "PoD.txt"
-	touch "Physics.txt"
+if [ ! -f "PoD.txt" ]; then touch "PoD.txt"; touch "Physics.txt"
 	echo | grep "PoD" "LCP_students.csv" > "PoD.txt"
 	echo | grep "Physics" "LCP_students.csv" > "Physics.txt"
 fi
 
 
-nummax=0
-res=("None")
-for i in {A..Z}
-do
+nummax=0; res=("None")
+for i in {A..Z}; do
 	temp=$(grep -c "^$i" "LCP_students.csv")
-	echo "$i"
-	echo "$temp"
-	echo ""
-	for x in ${res[@]}
-	do
-		if [ $temp -eq $nummax ]
-		then
-			res+=("$i")
+	echo "$i"; echo "$temp"; echo ""
+	for x in ${res[@]}; do
+		if [ $temp -eq $nummax ]; then res+=("$i")
 		elif [ $temp -gt $nummax ]
 		then
 			nummax=$temp
--- Lab_3/script_ex2.sh
+++ Lab_3/script_ex2.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Removing metadata and commas
-# grep -v "^#" "data.csv" | sed -e "s/,/ /g" > "data.txt"
+#grep -v "^#" "data.csv" | sed -e "s/,/ /g" > "data.txt"
 
 
 # Counting numbers in file
-#grep -oE "[1-9]+" data.txt
+even=0
+for i in $(grep -oE "[0-9]+" data.txt); do if ((  $i % 2 == 0 )); then ((even++)); fi; done; echo $even
 
 
 # Distinguishing entries
@@ -13,17 +14,11 @@ les=()
 gre=()
 thr=$(echo "100*sqrt(3)/2" | bc -l)
 for i in $(grep -oE "[1-9]+" < data.txt); do
-	if [ $(echo "$i < $thr" | bc -l) -eq 1 ]; then 
-		les+=("$i")
-	else 
-		gre+=("$i")
-	fi
+	if [ $(echo "$i < $thr" | bc -l) -eq 1 ]; then les+=("$i"); else gre+=("$i"); fi
 done
 echo "Numbers lesser than $thr: ${#les[@]}"
 echo "Numbers greater than $thr: ${#gre[@]}"
 
 
 # Copy n times dividing by n each iteration
-for i in $(seq 1 $1); do
-	echo "$(grep -oE "[1-9]+")/$i" | bc -l > data_$i.txt
-done
+for i in $(seq 1 $1); do echo "$(grep -oE "[1-9]+")/$i" | bc -l > data_$i.txt; done
